20a7c18681f6c5cdfe891a0cd2de4f5c5d64413c
Louis Garman <louisgarman@gmail.com>
2013-12-04 10:31:48 +0000

    Rails3 with mongoid, mongoid-history, and slim

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SidelinedRails3::Application.config.secret_token = 'aaf224e18f41a54516b820e6d2f16882da9669021e17591e4bda3b228bef4b378277677ab10604ccb7767916d72dba8ea91fb7dae8248c412d028e7bfee261cd'
