80cfc647ca6482141840bd9e0579e1993670a896
Louis Garman <louisgarman@gmail.com>
2014-01-10 19:10:38 +0000

    Rest client can't use http auth.

--- lib/sidelined/rest_client.rb
+++ lib/sidelined/rest_client.rb
@@ -7,41 +7,54 @@ module Sidelined
     #base_uri 'sidelined.herokuapp.com'
     base_uri 'sidelined.local'
 
-    attr_accessor :options
     WIKI_USER = 'wikipedia'
     WIKI_PASS = 'j843874q'
 
+    def initialize
+      options = {body: { user: { login: WIKI_USER, password: WIKI_PASS } } }
+      resp = HTTParty.post('http://sidelined.local/api/login', options)
+      parsed_resp = JSON.parse(resp.body)
+
+      unless parsed_resp.key?('success') and parsed_resp['success']
+        raise "Could not login"
+      end
+
+      @options = {
+        headers: {
+          'Content-Type' => 'application/json',
+          'Cookie' => resp.headers['set-cookie']
+        }
+      }
+    end
+
     class Fixture
       def self.post(fixture)
-        options = {:body => {:fixture => fixture}.to_json}
-        options.merge!(:basic_auth => {:username => WIKI_USER, :password => WIKI_PASS})
-        options.merge!(:headers => { 'Content-Type' => 'application/json' })
-
-        Sidelined::RestClient.post("/fixtures.json", options)
+        @options.merge!({:body => {:fixture => fixture}.to_json})
+        HTTParty.post("/fixtures.json", @options)
       end
     end
 
-     class Player
+    class Player
       def self.get(slug='')
-        Sidelined::RestClient.get("/players/#{slug}")
+        HTTParty.get("/players/#{slug}", @options)
       end
-      def self.post(player)
-        options = {:body => {:player => player}.to_json}
-        options.merge!(:basic_auth => {:username => WIKI_USER, :password => WIKI_PASS})
-        options.merge!(:headers => { 'Content-Type' => 'application/json' })
 
-        Sidelined::RestClient.post("/players.json", options)
+      def self.post(player)
+        @options.merge!({:body => {:player => player}.to_json})
+
+        HTTParty.post("/players.json", @options)
       end
+
       def self.put(slug, player)
-        options = {:body => player}
-        Sidelined::RestClient.put("/players/#{slug}", options)
+        @options.merge!({:body => {:player => player}.to_json})
+        HTTParty.put("/players/#{slug}", @options)
       end
     end
 
     class Club
       def self.get(id=nil)
         uri = id.nil? ? "/clubs.json" : "/clubs/#{id}.json"
-        resp = Sidelined::RestClient.get(uri)
+        resp = HTTParty.get(uri)
         JSON.parse(resp.body)
       end
       def self.post(club); end
