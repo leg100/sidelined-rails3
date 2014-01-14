0fba2c0628a91cc480a6e5149d8311fd99797af9
Louis Garman <louisgarman@gmail.com>
2014-01-14 21:57:58 +0000

    All auth paths belong in /api/users

--- config/routes.rb
+++ config/routes.rb
@@ -23,15 +23,21 @@ SidelinedRails3::Application.routes.draw do
     resources :history_trackers
    
     devise_scope :user do
-      get '/check-availability' => 'users/sessions#check_availability'
-      get '/current-user' => 'users/sessions#get_current_user'
-      post '/login' => 'users/sessions#create'
-      post '/logout' => 'users/sessions#destroy'
-      post '/signup' => 'users/registrations#create'
+      get 'check-availability' => 'users/sessions#check_availability'
+      get 'current-user' => 'users/sessions#get_current_user'
+
+      post 'users/sign_up' => 'users/registrations#create'
+      post 'users/sign_in' => 'users/sessions#create'
+      post 'users/sign_out' => 'users/sessions#destroy'
+      get 'users/confirmation' => 'users/confirmations#show'
     end
   end
 
-  devise_for :user
+  devise_for :user, :controllers => {
+    :registrations => "api/users/registrations",
+    :sessions => "api/users/sessions",
+    :confirmations => "api/users/confirmations" 
+  }
 
   match "api" => proc { [404, {}, ['Invalid API endpoint']] }
   match "api/*path" => proc { [404, {}, ['Invalid API endpoint']] }
@@ -40,6 +46,8 @@ SidelinedRails3::Application.routes.draw do
   resource :angular_root, :only => [:show]
   match 'injuries' => 'angular_root#show'
   match 'injuries/*ids' => 'angular_root#show'
+  match 'signup' => 'angular_root#show'
+  match 'confirmed' => 'angular_root#show'
   root to: 'angular_root#show'
 
   # the rest receive a 404.html
