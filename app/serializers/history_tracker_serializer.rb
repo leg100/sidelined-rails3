a266d3b93350fd4438dd4333a189c9b4286320c9
Louis Garman <louisgarman@gmail.com>
2014-01-23 10:13:06 +0000

    Missing modified content

--- app/serializers/history_tracker_serializer.rb
+++ app/serializers/history_tracker_serializer.rb
@@ -1,4 +1,4 @@
 class HistoryTrackerSerializer < ActiveModel::Serializer
-  attributes :id, :association_chain, :scope, :original, :updated_at, :created_at, :version, :action
+  attributes :id, :association_chain, :scope, :original, :modified, :updated_at, :created_at, :version, :action
   has_one :modifier
 end
