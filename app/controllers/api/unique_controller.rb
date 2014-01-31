ed51426debb15c495c198f95f8984371877bc84a
Louis Garman <louisgarman@gmail.com>
2014-01-31 13:36:21 +0000

    Controllers should be under /api

--- app/controllers/api/unique_controller.rb
+++ app/controllers/api/unique_controller.rb
@@ -1,10 +1,14 @@
-class UniqueController < ApplicationController
+class Api::UniqueController < ApplicationController
+ 
   # params: object, name, value
   def check_availability
     query = { params[:name] => params[:value] }
+    logger.debug query
     if params[:object].capitalize.constantize.send(:where, query).exists?
+      logger.debug "409"
       return render status: 409, json: { success: false, info: "#{params[:name]} taken." }
     else
+      logger.debug "200"
       return render status: 200, json: { success: true, info: 'available' } 
     end
   end
