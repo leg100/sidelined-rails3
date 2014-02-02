41d9c8992b024fd685c072dfe6bf1fe08be5a3c2
Louis Garman <louisgarman@gmail.com>
2014-02-02 09:08:46 +0000

    Use factory objects in testing.

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -6,20 +6,21 @@ feature 'User management' do
   end
 
   scenario "sign up", js: true do
+    user = build(:user)
     expect {
       visit signup_path
       within '.signup-form' do
-        fill_in 'signup_username', with: 'fanboy123'
-        fill_in 'email', with: 'newuser@example.com'
-        fill_in 'password', with: 'secret123'
-        fill_in 'passwordConfirmation', with: 'secret123'
+        fill_in 'signup_username', with: user.username
+        fill_in 'email', with: user.email
+        fill_in 'password', with: user.password
+        fill_in 'passwordConfirmation', with: user.password_confirmation
         click_button 'Sign up'
       end
       sleep 1
-      page.should_not have_content "fanboy123 is logged in"
+      page.should_not have_content "#{user.username} is logged in"
     }.to change(User, :count).by(1)
 
-    open_email('newuser@example.com')
+    open_email(user.email)
     current_email.default_part_body.to_s.should include('Confirm my account')
     visit_in_email('Confirm my account')
     expect(URI.parse(current_url).request_uri).to eq('/confirmed?status=success')
